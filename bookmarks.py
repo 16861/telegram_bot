bad42bb0eb2e0a2d44f6f19b770599ea4e0b50f9
16861 <benefacta@gmail.com>
2017-07-06 13:06:32 +0300

    add bookmarks module

---
+++ bookmarks.py
@@ -0,0 +1,46 @@
+class Bookmark():
+    '''
+    joined class for managing bookmarks and tags to them.
+    implement CRUD in 
+    '''
+    def __init__(self):
+        pass
+    #CRUD for bookmarks
+    #create bookmark
+    def createBookmarkQuery(self, url_, iduser = None, description=None, rate=None):
+        if not description:
+            description = ""
+        if not rate:
+            rate = 0
+        if not iduser:
+            iduser = 0
+        query = "insert into bookmarks(url, description, rate, iduser) values('{0}','{1}',{2},{3})".format(url_, description, rate, iduser)
+        return query
+
+    #read bookmark
+    def getBookmarks(self, tags=None, count=None):
+        if not count or type(count) != int :
+            query = "SELECT url, description, rate, id FROM bookmarks "
+        else:
+            query = "SELECT url, description, rate, id FROM bookmarks LIMIT {0}".format(count)
+        return query
+
+    #update bookmark
+    def updateBookmarkQuery(self, id, new_data, type='url'):
+        if type == "url":
+            query = "UPDATE bookmarks SET url = {0} WHERE id = {1}".format(new_data, id)
+        elif type == "description":
+            query = "UPDATE bookmarks SET description = {0} WHERE id = {1}".format(new_data, id)
+        elif type == "rate":
+            query = "UPDATE bookmarks SET rate = {0} WHERE id = {1}".format(new_data, id)
+        else:
+            return None
+        return query
+
+    #delete bookmark
+    def deleteBookmarkQuery(self, id):
+        query = "DELETE FROM bookmarks WHERE id = {0}".format(id)
+        return query
+
+    #Tags section
+    #CRUD for tags
